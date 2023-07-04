8b0912ef57998ca2a713dd9be1bee0dd7d22b2a1
Julian Baldwin <jnbaldwin@g002.ds.uchicago.edu>
2023-07-04 14:29:31 -0500

    initial changes

---
+++ produce_probes.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for X in {1..8}
+do
+    echo "probe layer $X"
+    # python3 train_probe_othello.py --layer $X --twolayer --mid_dim 256 --ckpt $C
+    python3 train_probe_othello.py --layer $X --twolayer --mid_dim 256 --random
+done
