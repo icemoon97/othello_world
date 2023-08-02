2964d007ffbbd8aee1cca1ab2168fa9bbb2f46d1
Julian Baldwin <julianbaldwin97@gmail.com>
2023-08-01 21:47:35 -0500

    lots of work with linear board interventions

--- produce_probes.sh
+++ produce_probes.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for X in {1..8}
+for X in 1 2 3 4 5 6 7 8
 do
     echo "$C probe layer $X"
-    python3 train_probe_othello.py --layer $X --twolayer --mid_dim 256 --ckpt bias/finetune/ft_bias80_100_e10
+    python3 train_probe_othello.py --layer $X --ckpt bias/TLcontrol --type state
     # python3 train_probe_othello.py --layer $X --twolayer --mid_dim 256 --random
 done
